32565695470deea7314a998abec85d5af619dd90
Zain-ul-abdin0 <ahmza147@gmail.com>
2025-04-20 13:27:59 +0200

    Filter even numbers from list

--- TypeScript/src/index.ts
+++ TypeScript/src/index.ts
@@ -1,7 +1,19 @@
-function sumOfAnArray(arr: number[]): number {
-  let sum = 0;
-  sum = arr.reduce((sum,arr)=>sum+arr);
-  return sum;
-}
+// function sumOfAnArray(arr: number[]): number {
+//   let sum = 0;
+//   sum = arr.reduce((sum,arr)=>sum+arr);
+//   return sum;
+// }
 
-console.log(sumOfAnArray([1, 2, 3, 4, 5]));
+// console.log(sumOfAnArray([1, 2, 3, 4, 5]));
+
+// function reverseString(str: string):string{
+//   let result : string = str;
+//   return result.split('').reverse().join('');
+// }
+// console.log(reverseString('ahmad'));
+
+
+function FilterEvenNumbersFromList(arr:number[]):number[]{
+  return arr.filter((arr=>arr%2==0));
+}
+console.log(FilterEvenNumbersFromList([1,2,4,5]))
